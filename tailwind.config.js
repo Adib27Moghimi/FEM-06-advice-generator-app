1e4eab108c599dce3a31603e3af448906face624
Adib Moghimi <adib1991moghimi@gmail.com>
2024-06-12 17:57:54 +0330

    Install and configure Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js", "./index.html"],
+  theme: {
+    screens: {
+      max375: { max: "375px" },
+      mobile2: "375px",
+      laptop: "1024px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          cyan: {
+            light: "hsl(193, 38%, 86%)",
+          },
+          green: {
+            neon: " hsl(150, 100%, 66%)",
+          },
+        },
+        neutral: {
+          blue: {
+            grayish: {
+              DEFAULT: " hsl(217, 19%, 38%)",
+              dark: "hsl(217, 19%, 24%)",
+            },
+            dark: "hsl(218, 23%, 16%)",
+          },
+        },
+      },
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+    fontSize: {
+      optical: "auto",
+    },
+    fontWeight: {
+      800: "800",
+    },
+  },
+  plugins: [],
+};
